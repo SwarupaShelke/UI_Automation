89d28dbba258557505c6931aaa798731371f556d
Swarupa Shelke <swarupashelke91@gmail.com>
2025-12-08 19:59:15 +0530

    added explicit wait

--- src/main/java/Pages/UserInformationPage.java
+++ src/main/java/Pages/UserInformationPage.java
@@ -1,11 +1,16 @@
 package Pages;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class UserInformationPage {
 	
 	WebDriver driver;
+	WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 	
 	By firstname =By.id("first-name");
 	By lastname =By.id("last-name");
@@ -18,18 +23,23 @@ public class UserInformationPage {
 	}
 	
 	public void enterFirstName(String user) {
+		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(firstname));
 		driver.findElement(firstname).sendKeys("standard");
 	}
 	
 	public void enterlastName(String user) {
+		wait.until(ExpectedConditions.visibilityOfElementLocated(lastname));
 		driver.findElement(lastname).sendKeys("user");
 	}
 	
 	public void enterPostCode(String password) {
+		wait.until(ExpectedConditions.visibilityOfElementLocated(postalcode));
 		driver.findElement(postalcode).sendKeys("411014");
 	}
 	
 	public void clickContinueButton() {
+		wait.until(ExpectedConditions.visibilityOfElementLocated(continuebutton));
 		driver.findElement(continuebutton).click();
 	}
 	
