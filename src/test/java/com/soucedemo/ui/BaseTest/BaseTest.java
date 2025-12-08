b860e424060edd28540936da802015194adcdb86
Swarupa Shelke <swarupashelke91@gmail.com>
2025-12-08 19:21:42 +0530

    added chromeoption

--- src/test/java/com/soucedemo/ui/BaseTest/BaseTest.java
+++ src/test/java/com/soucedemo/ui/BaseTest/BaseTest.java
@@ -1,6 +1,8 @@
 package com.soucedemo.ui.BaseTest;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
@@ -18,8 +20,11 @@ public class BaseTest {
 		configReader.initProp();
 		
 		String url = configReader.getProperty("baseURL");
-		
-		driver= new EdgeDriver();
+		ChromeOptions option =new ChromeOptions();
+		option.addArguments("--headless=new");
+		option.addArguments("--no-sandbox");
+		option.addArguments("--disable-dev-shm-usage");
+		driver= new ChromeDriver(option);
 		driver.manage().window().maximize();
 		driver.get(url);
 	}
